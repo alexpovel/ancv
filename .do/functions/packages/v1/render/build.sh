f5e6a01926085b2c06e379dda45f71632e87f15b
Alex Povel <git@alexpovel.de>
2022-07-17 01:24:41 +0200

    wip requirements

--- .do/functions/packages/v1/render/build.sh
+++ .do/functions/packages/v1/render/build.sh
@@ -2,5 +2,8 @@
 
 set -e
 
+echo "In build script"
 virtualenv --without-pip virtualenv
+echo "Ran virtualenv"
 pip install -r requirements.txt --target virtualenv/lib/python3.9/site-packages
+echo "Ran ran"
