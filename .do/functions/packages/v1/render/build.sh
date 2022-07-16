43625ad4bdb2a7c1dd93d1cd2e27fa12a8c2776c
Alex Povel <git@alexpovel.de>
2022-07-17 00:57:22 +0200

    wip requirements

---
+++ .do/functions/packages/v1/render/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# set -e
+
+pip install -r requirements.txt
+
+# virtualenv --without-pip virtualenv
+# pip install -r requirements.txt --target virtualenv/lib/python3.9/site-packages
