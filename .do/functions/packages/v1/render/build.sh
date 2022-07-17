8c6dc629bf55f2ca9fcddb54ea5a869a9b7a6e78
Alex Povel <git@alexpovel.de>
2022-07-17 09:31:34 +0200

    wip requirements

--- .do/functions/packages/v1/render/build.sh
+++ .do/functions/packages/v1/render/build.sh
@@ -7,11 +7,17 @@
 
 # id
 
-virtualenv --without-pip virtualrofl
+virtualenv virtualenv
+source virtualenv/bin/activate
+pip install pyjokes
+# pip install -r requirements.txt
+deactivate
+
+# virtualenv --without-pip virtualrofl
 # pip install --user ancv
 
 # pip install -r requirements --target virtualenv/lib/python3.9/site-packages
-pip install pyjokes --target virtualrofl/lib/python3.9/site-packages
+# pip install pyjokes --target virtualrofl/lib/python3.9/site-packages
 # source virtualenv/bin/activate
 
 # echo "In build script"
