b303b9afccb048322ba62245c87b0deab08ad128
Alex Povel <git@alexpovel.de>
2022-07-26 19:47:44 +0200

    chore(tests): Implement some initial CLI tests
    See also #7, #8

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,3 +2,5 @@ from pathlib import Path
 
 TESTS_DIR = Path(__file__).parent
 DATA_DIR = TESTS_DIR / "data"
+RESUMES_DIR = DATA_DIR / "resumes"
+RESUMES = list(RESUMES_DIR.iterdir())
---
+++ tests/test_cli.py
@@ -0,0 +1,40 @@
+import pytest
+from typer.testing import CliRunner
+
+from ancv.__main__ import app
+from tests import RESUMES
+
+RUNNER = CliRunner()
+
+
+def test_help_exists():
+    result = RUNNER.invoke(app, ["--help"])
+    assert result.exit_code == 0
+
+
+def test_serve_exists():
+    result = RUNNER.invoke(app, ["serve"])
+    assert result.exit_code == 0
+
+
+def test_serve_file_exists():
+    result = RUNNER.invoke(app, ["serve", "file", "--help"])
+    assert result.exit_code == 0
+
+
+def test_serve_api_exists():
+    result = RUNNER.invoke(app, ["serve", "api", "--help"])
+    assert result.exit_code == 0
+
+
+@pytest.mark.parametrize("filename", RESUMES)
+# All resumes as a single fixture wouldn't be too bad either but doesn't work:
+# https://stackoverflow.com/q/56672179/11477374
+class TestCli:
+    def test_validate(self, filename):
+        result = RUNNER.invoke(app, ["validate", str(filename)])
+        assert result.exit_code == 0
+
+    def test_render(self, filename):
+        result = RUNNER.invoke(app, ["render", str(filename)])
+        assert result.exit_code == 0
