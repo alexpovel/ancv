9896f1ebd8d399ad4dfe7613dab944cb45875213
Alex Povel <git@alexpovel.de>
2022-07-26 20:00:45 +0200

    chore(tests): Adjust tests directory structure to match project

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
 TESTS_DIR = Path(__file__).parent
-DATA_DIR = TESTS_DIR / "data"
+DATA_DIR = TESTS_DIR / "test_data"
 RESUMES_DIR = DATA_DIR / "resumes"
 RESUMES = list(RESUMES_DIR.iterdir())
