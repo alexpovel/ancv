32fd16b3b0ef0e0b86f3c1267798dabccebcc521
Alex Povel <git@alexpovel.de>
2022-08-13 22:18:02 +0200

    test: Extract GitHub token for tests
    Tests can quickly reach the rate limit if not authenticated.

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,6 +1,9 @@
+import os
 from pathlib import Path
 
 TESTS_DIR = Path(__file__).parent
 DATA_DIR = TESTS_DIR / "test_data"
 RESUMES_DIR = DATA_DIR / "resumes"
 RESUMES = {p.name: p for p in RESUMES_DIR.iterdir()}
+
+GH_TOKEN = os.environ.get("GH_TOKEN", None)
--- tests/web/conftest.py
+++ tests/web/conftest.py
@@ -1,18 +1,16 @@
-import os
-
 import pytest
 from aiohttp.web import Application
 
 from ancv.reflection import METADATA
 from ancv.web.server import APIHandler, FileHandler
-from tests import RESUMES
+from tests import GH_TOKEN, RESUMES
 
 
 @pytest.fixture(scope="function")
 def api_client_app() -> Application:
     return APIHandler(
         requester=f"{METADATA.name}-PYTEST-REQUESTER",
-        token=os.environ.get("GH_TOKEN", None),
+        token=GH_TOKEN,
         homepage=f"{METADATA.name}-PYTEST-HOMEPAGE",
         landing_page=f"{METADATA.name}-PYTEST-LANDING_PAGE",
     ).app
