fb58c583234dd8fb77e1a96ab8371033a15e3cc7
Alex Povel <git@alexpovel.de>
2022-09-18 12:04:41 +0200

    fix: Please the Windows overlords
    On Windows, `print` (which uses `sys.stdout` per default) defaults to `cp1252`
    when piping, breaking e.g. rip-grep and all other Unicode-based tools.

--- ancv/__init__.py
+++ ancv/__init__.py
@@ -1,8 +1,12 @@
+import sys
 from enum import IntEnum
 from pathlib import Path
 
 PROJECT_ROOT = Path(__file__).parent
 PACKAGE = __package__
+
+# See also https://github.com/python/typeshed/issues/3049
+sys.stdout.reconfigure(encoding="utf-8")  # type: ignore[attr-defined]
 
 
 class SIPrefix(IntEnum):
