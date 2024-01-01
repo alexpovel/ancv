3b6dc0137c6468f2c7eb4d2aed4bf2004e7e475a
Alex Povel <git@alexpovel.de>
2024-01-01 15:20:33 +0100

    chore: Move `Field` into `typing.Annotated`
    Supports field defaults natively now

--- ancv/data/models/github.py
+++ ancv/data/models/github.py
@@ -1,7 +1,7 @@
 """This module contains models for the relevant parts of GitHub's API."""
 
+import typing as t
 from datetime import datetime
-from typing import Mapping, Optional
 
 from pydantic import BaseModel, Field, HttpUrl
 
@@ -12,11 +12,11 @@ class File(BaseModel):
     See: https://docs.github.com/en/rest/gists/gists?apiVersion=2022-11-28#list-gists-for-the-authenticated-user, under the "files" key.
     """
 
-    filename: Optional[str] = None
-    type: Optional[str] = None
-    language: Optional[str] = None
-    raw_url: Optional[HttpUrl] = None
-    size: Optional[int] = None
+    filename: t.Optional[str] = None
+    type: t.Optional[str] = None
+    language: t.Optional[str] = None
+    raw_url: t.Optional[HttpUrl] = None
+    size: t.Optional[int] = None
 
 
 class GistUser(BaseModel):
@@ -25,49 +25,54 @@ class GistUser(BaseModel):
     See: https://docs.github.com/en/rest/gists/gists?apiVersion=2022-11-28#list-gists-for-the-authenticated-user
     """
 
-    name: Optional[str] = None
-    email: Optional[str] = None
-    login: str = Field(..., examples=["octocat"])
-    id: int = Field(..., examples=[1])
-    node_id: str = Field(..., examples=["MDQ6VXNlcjE="])
-    avatar_url: HttpUrl = Field(
-        ..., examples=["https://github.com/images/error/octocat_happy.gif"]
-    )
-    gravatar_id: Optional[str] = Field(
-        ..., examples=["41d064eb2195891e12d0413f63227ea7"]
-    )
-    url: HttpUrl = Field(..., examples=["https://api.github.com/users/octocat"])
-    html_url: HttpUrl = Field(..., examples=["https://github.com/octocat"])
-    followers_url: HttpUrl = Field(
-        ..., examples=["https://api.github.com/users/octocat/followers"]
-    )
-    following_url: str = Field(
-        ..., examples=["https://api.github.com/users/octocat/following{/other_user}"]
-    )
-    gists_url: str = Field(
-        ..., examples=["https://api.github.com/users/octocat/gists{/gist_id}"]
-    )
-    starred_url: str = Field(
-        ..., examples=["https://api.github.com/users/octocat/starred{/owner}{/repo}"]
-    )
-    subscriptions_url: HttpUrl = Field(
-        ..., examples=["https://api.github.com/users/octocat/subscriptions"]
-    )
-    organizations_url: HttpUrl = Field(
-        ..., examples=["https://api.github.com/users/octocat/orgs"]
-    )
-    repos_url: HttpUrl = Field(
-        ..., examples=["https://api.github.com/users/octocat/repos"]
-    )
-    events_url: str = Field(
-        ..., examples=["https://api.github.com/users/octocat/events{/privacy}"]
-    )
-    received_events_url: HttpUrl = Field(
-        ..., examples=["https://api.github.com/users/octocat/received_events"]
-    )
-    type: str = Field(..., examples=["User"])
+    name: t.Optional[str] = None
+    email: t.Optional[str] = None
+    login: t.Annotated[str, Field(examples=["octocat"])]
+    id: t.Annotated[int, Field(examples=[1])]
+    node_id: t.Annotated[str, Field(examples=["MDQ6VXNlcjE="])]
+    avatar_url: t.Annotated[
+        HttpUrl, Field(examples=["https://github.com/images/error/octocat_happy.gif"])
+    ]
+    gravatar_id: t.Annotated[
+        t.Optional[str], Field(examples=["41d064eb2195891e12d0413f63227ea7"])
+    ]
+    url: t.Annotated[HttpUrl, Field(examples=["https://api.github.com/users/octocat"])]
+    html_url: t.Annotated[HttpUrl, Field(examples=["https://github.com/octocat"])]
+    followers_url: t.Annotated[
+        HttpUrl, Field(examples=["https://api.github.com/users/octocat/followers"])
+    ]
+    following_url: t.Annotated[
+        str,
+        Field(examples=["https://api.github.com/users/octocat/following{/other_user}"]),
+    ]
+    gists_url: t.Annotated[
+        str, Field(examples=["https://api.github.com/users/octocat/gists{/gist_id}"])
+    ]
+    starred_url: t.Annotated[
+        str,
+        Field(examples=["https://api.github.com/users/octocat/starred{/owner}{/repo}"]),
+    ]
+    subscriptions_url: t.Annotated[
+        HttpUrl, Field(examples=["https://api.github.com/users/octocat/subscriptions"])
+    ]
+    organizations_url: t.Annotated[
+        HttpUrl, Field(examples=["https://api.github.com/users/octocat/orgs"])
+    ]
+    repos_url: t.Annotated[
+        HttpUrl, Field(examples=["https://api.github.com/users/octocat/repos"])
+    ]
+    events_url: t.Annotated[
+        str, Field(examples=["https://api.github.com/users/octocat/events{/privacy}"])
+    ]
+    received_events_url: t.Annotated[
+        HttpUrl,
+        Field(examples=["https://api.github.com/users/octocat/received_events"]),
+    ]
+    type: t.Annotated[str, Field(examples=["User"])]
     site_admin: bool
-    starred_at: Optional[datetime] = Field(None, examples=['"2020-07-09T00:17:55Z"'])
+    starred_at: t.Annotated[
+        t.Optional[datetime], Field(None, examples=['"2020-07-09T00:17:55Z"'])
+    ]
 
 
 class Gist(BaseModel):
@@ -83,13 +88,13 @@ class Gist(BaseModel):
     git_pull_url: HttpUrl
     git_push_url: HttpUrl
     html_url: HttpUrl
-    files: Mapping[str, File]
+    files: t.Mapping[str, File]
     public: bool
     created_at: datetime
     updated_at: datetime
-    description: Optional[str] = None
+    description: t.Optional[str] = None
     comments: int
-    user: Optional[GistUser] = None
+    user: t.Optional[GistUser] = None
     comments_url: HttpUrl
-    owner: Optional[GistUser] = None
-    truncated: Optional[bool] = None
+    owner: t.Optional[GistUser] = None
+    truncated: t.Optional[bool] = None
--- ancv/reflection.py
+++ ancv/reflection.py
@@ -1,7 +1,7 @@
+import typing as t
 from importlib.metadata import metadata
-from typing import Optional
 
-from pydantic import field_validator, AnyUrl, BaseModel, EmailStr, Field
+from pydantic import AnyUrl, BaseModel, EmailStr, Field, field_validator
 
 from ancv import PACKAGE
 
@@ -19,54 +19,92 @@ class Metadata(BaseModel):
         - https://peps.python.org/pep-0566/
     """
 
-    metadata_version: str = Field(
-        description="Version of the metadata format, e.g. '2.1'",
-    )
-    name: str = Field(
-        description="Name of the package, e.g. 'ancv'",
-        pattern=r"(?i)^([A-Z0-9]|[A-Z0-9][A-Z0-9._-]*[A-Z0-9])$",
-    )
-    version: str = Field(
-        description="Version of the package, e.g. '0.1.0'",
-        # https://peps.python.org/pep-0440/#appendix-b-parsing-version-strings-with-regular-expressions
-        pattern=r"^([1-9][0-9]*!)?(0|[1-9][0-9]*)(\.(0|[1-9][0-9]*))*((a|b|rc)(0|[1-9][0-9]*))?(\.post(0|[1-9][0-9]*))?(\.dev(0|[1-9][0-9]*))?$",
-    )
-    summary: Optional[str] = Field(
-        None, description="One-line summary of the package, e.g. 'Ancv is a package for ...'",
-    )
-    home_page: Optional[AnyUrl] = Field(
-        None, description="Homepage of the package, e.g. https://ancv.io/'"
-    )
-    download_url: Optional[AnyUrl] = Field(
-        None, description="URL to download this version of the package"
-    )
-    license: Optional[str] = Field(None, description="License of the package, e.g. 'MIT'")
-    author: Optional[str] = Field(None, description="Author of the package, e.g. 'John Doe'")
-    author_email: Optional[EmailStr] = Field(
-        None, description="Email of the author, e.g. john@doe.com'"
-    )
-    requires_python: Optional[str] = Field(
-        None, description="Python version required by the package, e.g. '>=3.6'",
-    )
-    classifier: Optional[list[str]] = Field(
-        None, description="Classifiers of the package, e.g. 'Programming Language :: Python :: 3.6'",
-    )
-    requires_dist: Optional[list[str]] = Field(
-        None, description="Distributions required by the package, e.g. 'aiohttp[speedups] (>=3.8.1,<4.0.0)'",
-    )
-    project_url: Optional[list[str]] = Field(
-        None, description="Project URLs of the package, e.g. 'Repository, https://github.com/namespace/ancv/'",
-    )
-    description_content_type: Optional[str] = Field(
-        None, description="Content type of the description, e.g. 'text/plain'",
-    )
-    description: Optional[str] = Field(
-        None, description="Long description of the package, e.g. 'Ancv is a package for ...'",
-    )
+    metadata_version: t.Annotated[
+        str,
+        Field(
+            description="Version of the metadata format, e.g. '2.1'",
+        ),
+    ]
+    name: t.Annotated[
+        str,
+        Field(
+            description="Name of the package, e.g. 'ancv'",
+            pattern=r"(?i)^([A-Z0-9]|[A-Z0-9][A-Z0-9._-]*[A-Z0-9])$",
+        ),
+    ]
+    version: t.Annotated[
+        str,
+        Field(
+            description="Version of the package, e.g. '0.1.0'",
+            # https://peps.python.org/pep-0440/#appendix-b-parsing-version-strings-with-regular-expressions
+            pattern=r"^([1-9][0-9]*!)?(0|[1-9][0-9]*)(\.(0|[1-9][0-9]*))*((a|b|rc)(0|[1-9][0-9]*))?(\.post(0|[1-9][0-9]*))?(\.dev(0|[1-9][0-9]*))?$",
+        ),
+    ]
+    summary: t.Annotated[
+        t.Optional[str],
+        Field(
+            description="One-line summary of the package, e.g. 'Ancv is a package for ...'",
+        ),
+    ] = None
+    home_page: t.Annotated[
+        t.Optional[AnyUrl],
+        Field(description="Homepage of the package, e.g. https://ancv.io/'"),
+    ] = None
+    download_url: t.Annotated[
+        t.Optional[AnyUrl],
+        Field(description="URL to download this version of the package"),
+    ] = None
+    license: t.Annotated[
+        t.Optional[str], Field(description="License of the package, e.g. 'MIT'")
+    ] = None
+    author: t.Annotated[
+        t.Optional[str],
+        Field(description="Author of the package, e.g. 'John Doe'"),
+    ] = None
+    author_email: t.Annotated[
+        t.Optional[EmailStr],
+        Field(description="Email of the author, e.g. john@doe.com'"),
+    ] = None
+    requires_python: t.Annotated[
+        t.Optional[str],
+        Field(
+            description="Python version required by the package, e.g. '>=3.6'",
+        ),
+    ] = None
+    classifier: t.Annotated[
+        t.Optional[list[str]],
+        Field(
+            description="Classifiers of the package, e.g. 'Programming Language :: Python :: 3.6'",
+        ),
+    ] = None
+    requires_dist: t.Annotated[
+        t.Optional[list[str]],
+        Field(
+            description="Distributions required by the package, e.g. 'aiohttp[speedups] (>=3.8.1,<4.0.0)'",
+        ),
+    ] = None
+    project_url: t.Annotated[
+        t.Optional[list[str]],
+        Field(
+            description="Project URLs of the package, e.g. 'Repository, https://github.com/namespace/ancv/'",
+        ),
+    ] = None
+    description_content_type: t.Annotated[
+        t.Optional[str],
+        Field(
+            description="Content type of the description, e.g. 'text/plain'",
+        ),
+    ] = None
+    description: t.Annotated[
+        t.Optional[str],
+        Field(
+            description="Long description of the package, e.g. 'Ancv is a package for ...'",
+        ),
+    ] = None
 
     @field_validator("project_url")
     @classmethod
-    def strip_prefix(cls, v: Optional[list[str]]) -> Optional[list[str]]:
+    def strip_prefix(cls, v: t.Optional[list[str]]) -> t.Optional[list[str]]:
         """Strips the prefixes 'Name, ' from the URLs.
 
         For example, extracts just the URL from:
@@ -77,4 +115,4 @@ class Metadata(BaseModel):
         return [url.split()[-1] for url in v]
 
 
-METADATA = Metadata.parse_obj(metadata(PACKAGE).json)
+METADATA = Metadata.model_validate(metadata(PACKAGE).json)
